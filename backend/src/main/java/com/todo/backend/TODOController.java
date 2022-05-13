76cb7aed4ede311411a12b9726da503d5dbd8833
vDawgg <veit.laule@gmail.com>
2022-05-13 16:33:49 +0000

    All done except for method returning every todo.

--- backend/src/main/java/com/todo/backend/TODOController.java
+++ backend/src/main/java/com/todo/backend/TODOController.java
@@ -1,39 +1,51 @@
 package com.todo.backend;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@SpringBootApplication
+@EnableMongoRepositories(basePackageClasses = TODORepository.class)
 public class TODOController {
 
+    @Autowired
+    private TODORepository repository;
+
+    //Get all todos
     @GetMapping("allTodos")
     public TODO[] allTodos() {
         return null;
     }
 
+    //Get specific todos
     @GetMapping("todo")
     public TODO todo(@RequestParam("id") int id) {
-        return new TODO(id, "Backend fertig machen", "2022/13/05", 0);
+        return repository.findById(id);
     }
 
     //Post a todo
-    @PostMapping("todos")
-    public boolean addTODO() {
-        return false;
+    @PostMapping("todo")
+    public boolean addTODO(@RequestBody TODO todo) {
+        try {
+            repository.save(todo);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
     }
 
     //Delete an existing todo
     @GetMapping("delete")
     public boolean delete(@RequestParam("id") int id) {
-        return false;
+        repository.deleteById(id);
+        return true;
     }
 
     //Edit an existing todo
     @PostMapping("edit")
-    public TODO edit(@RequestParam("id") int id) {
-        return null;
+    public TODO edit(@RequestBody TODO todo) {
+        return repository.save(todo);
     }
-
 }
---
+++ backend/src/main/java/com/todo/backend/TODORepository.java
@@ -0,0 +1,11 @@
+package com.todo.backend;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TODORepository extends MongoRepository<TODO, String> {
+
+    public TODO findById(int id);
+
+    //Might have to be more specific -> What if user tries to delete non-existent todos?
+    public void deleteById(int id);
+}
