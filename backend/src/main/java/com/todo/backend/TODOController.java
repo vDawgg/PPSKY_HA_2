b7cdb80130a75d7e2c16b9f9783b8103ffab9906
vDawgg <veit.laule@gmail.com>
2022-05-21 18:46:46 +0200

    Added Cross Origin header

--- backend/src/main/java/com/todo/backend/TODOController.java
+++ backend/src/main/java/com/todo/backend/TODOController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @EnableMongoRepositories(basePackageClasses = TODORepository.class)
 public class TODOController {
